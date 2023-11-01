ecc52caf3d3ba1c77589a1b4a9c29c761273fd87
MiniMestri <fonsild97@hotmail.com>
2023-11-01 19:14:24 +0100

    Funcionamiento Programa
    He desarrollado el código para que el usuario elija alguna opción con el FileChooser y copie la rute en ul textField además de añadir el funcionamiento de los botones para el uso de la consola

--- Practica3DI/src/practica3di/Practica3DI.java
+++ Practica3DI/src/practica3di/Practica3DI.java
@@ -1,5 +1,8 @@
 package practica3di;
 
+import java.io.File;
+import javax.swing.JFileChooser;
+
 /**
  *
  * @author fonsi
@@ -21,7 +24,6 @@ public class Practica3DI extends javax.swing.JFrame {
         jTextField1 = new javax.swing.JTextField();
         jTextField2 = new javax.swing.JTextField();
         jTextField3 = new javax.swing.JTextField();
-        jPanel1 = new javax.swing.JPanel();
         jFileChooser1 = new javax.swing.JFileChooser();
         jComboBox1 = new javax.swing.JComboBox<>();
         jLabel5 = new javax.swing.JLabel();
@@ -29,12 +31,6 @@ public class Practica3DI extends javax.swing.JFrame {
         jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        addWindowListener(new java.awt.event.WindowAdapter() {
-            public void windowOpened(java.awt.event.WindowEvent evt) {
-                formWindowOpened(evt);
-            }
-        });
 
         jButton1.setText("...");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -59,9 +55,6 @@ public class Practica3DI extends javax.swing.JFrame {
         jLabel4.setText("CONVERSOR DE VIDEOS");
         jLabel6.setIcon(new javax.swing.ImageIcon("C:\\Users\\fonsi\\Desktop\\ESTUDIO\\IMF 2\\DESARROLLO DE INTERFACES\\Practicas\\Practica3DI\\img\\logo.jpg"));
 
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        jPanel1.add(jFileChooser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 470, 270));
-
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[]{"avi", "mp4", "mkv"}));
 
         jLabel5.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
@@ -69,9 +62,19 @@ public class Practica3DI extends javax.swing.JFrame {
 
         jButton3.setFont(new java.awt.Font("Impact", 0, 24)); // NOI18N
         jButton3.setText("INICIO");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setFont(new java.awt.Font("Impact", 0, 24)); // NOI18N
         jButton4.setText("BORRAR");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -99,8 +102,7 @@ public class Practica3DI extends javax.swing.JFrame {
                                                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                                                                 .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                                                 .addComponent(jLabel5)))
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
-                                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 480, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE))
                                         .addGroup(layout.createSequentialGroup()
                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                                         .addGroup(layout.createSequentialGroup()
@@ -146,8 +148,7 @@ public class Practica3DI extends javax.swing.JFrame {
                                                 .addGap(18, 18, 18)
                                                 .addComponent(jLabel5)
                                                 .addGap(18, 18, 18)
-                                                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                 .addGap(45, 45, 45)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                         .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, 56, Short.MAX_VALUE)
@@ -158,15 +159,54 @@ public class Practica3DI extends javax.swing.JFrame {
         pack();
     }
 
-    private void formWindowOpened(java.awt.event.WindowEvent evt) {
-        jPanel1.setVisible(false);
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+        jFileChooser1.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+        int resultado = jFileChooser1.showOpenDialog(this);
+        if (resultado != JFileChooser.CANCEL_OPTION) {
+            File ruta = jFileChooser1.getSelectedFile();
+
+            jTextField2.setText(ruta.getAbsolutePath());
+
+        }
+
     }
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
-        jPanel1.setVisible(true);
-    }   
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                         
-        jPanel1.setVisible(true);
-    }   
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {
+
+        jFileChooser1.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+        int resultado = jFileChooser1.showOpenDialog(this);
+        if (resultado != JFileChooser.CANCEL_OPTION) {
+            File ruta = jFileChooser1.getSelectedFile();
+
+            jTextField3.setText(ruta.getAbsolutePath());
+
+        }
+    }
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {
+        String programa = jTextField1.getText();
+        String entrada = jTextField2.getText();
+        String salida = jTextField3.getText();
+        String formato = jComboBox1.getSelectedItem().toString();
+        String comando = programa + " -i " + entrada + " " + salida + "." + formato;
+
+        try {
+            ProcessBuilder proceso = new ProcessBuilder("cmd.exe", "/c", comando);
+            Process miProceso = proceso.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {
+        jTextField1.setText("");
+        jTextField2.setText("");
+        jTextField3.setText("");
+    }
 
     public static void main(String[] args) {
 
@@ -188,7 +228,6 @@ public class Practica3DI extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
